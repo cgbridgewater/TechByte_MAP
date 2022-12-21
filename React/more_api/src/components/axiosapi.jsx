2e55645282fb9b00228126f02d24ff1110fb540a
cgbridgewater <cgbridgewater@outlook.com>
2022-12-21 00:15:55 -0800

    updating code to make api call button

--- React/more_api/src/components/axiosapi.jsx
+++ React/more_api/src/components/axiosapi.jsx
@@ -3,18 +3,24 @@ import React, { useEffect, useState } from "react";
 
 const GetChuck = (props) => {
 
+    const [ chuck, setChuck ] = useState(false);
     const [ chuckJoke, setChuckJoke ] = useState(null);
 
-    useEffect(() => {
-        axios
-            .get('https://api.chucknorris.io/jokes/random')
-            // .then((response) => console.log("ssss", response.data.value))  <=== use this to test what you're getting back from the api!!!!
-            .then(response=>setChuckJoke(response.data.value))
-    }, []); //empty array forces useEffect to render ONLY when the component first renders!
+    const getData = (e) => {
+        e.preventDefault();
+            axios
+                .get('https://api.chucknorris.io/jokes/random')
+                // .then((response) => console.log("ssss", response.data.value))  <=== use this to test what you're getting back from the api!!!!
+                .then(response=>setChuckJoke(response.data.value))
+    }; 
 
     return (
         <div className="ChuckNorris">
-            <h3>Tell me a joke about the Great Chuck Norris</h3>
+            <h3>Feeling Lucky On The ChuckNorris API?</h3>
+            <button className="ChuckButton"
+            onClick={getData}
+            type="button"
+            >Yeah, Roll The Dice!</button>
             <p>
             {chuckJoke}
             </p>
