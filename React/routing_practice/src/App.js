a5d42f60358f1be45d91a2a2fc9f364222f5057b
cgbridgewater <cgbridgewater@outlook.com>
2022-12-21 21:36:29 -0800

    updating routes and style

--- React/routing_practice/src/App.js
+++ React/routing_practice/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from './components/Index';
+import Home from './components/Home';
+import Four from './components/4';
+import Hello from './components/Hello';
+import HelloColors from './components/HelloColors';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+      <div className="App">
+
+        <Routes>
+
+          <Route path='/' element={<Index /> }/>
+          <Route path='/home' element={<Home /> }/>
+          <Route path='/4' element={<Four /> }/>
+          <Route path='/hello' element={<Hello /> }/>
+          <Route path='/hello' element={<Hello /> }/>
+          <Route path='/hello/red/blue' element={<HelloColors /> }/>
+          
+        </Routes>
+  
+      </div>
+    </BrowserRouter>
   );
 }
 
