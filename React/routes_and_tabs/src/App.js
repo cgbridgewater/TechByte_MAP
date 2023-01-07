d5ee196a8d606f9b8ef6dd685ad7a58277e0acb1
cgbridgewater <cgbridgewater@outlook.com>
2023-01-07 00:22:48 -0800

    updates to css and layout

--- React/routes_and_tabs/src/App.js
+++ React/routes_and_tabs/src/App.js
@@ -1,63 +1,28 @@
 import './App.css';
 import { useState } from 'react';
-import styled from 'styled-components';
-// import { Tab, Tabs, TabPanel } from './components/tabs/tabs';
-import Tab from './components/Tab'; 
-import Tabs from './components/Tabs';
-import TabPanel from './components/TabPanel';
+import APIWalkerPeopleDisplay from './components/tabDisplays/PeopleDisplay'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import TabDisplay from './components/TabDisplay';
 
-
-const TabsContainer = styled.div`
-  display: flex;
-  padding: 2px;
-`;
-
-const TabPanelContainer = styled.div`
-height: 100vh;
-`;
 
 function App() {
 
-  const [activeTab, setActiveTab] = useState(1);
-
-  const handleChange = (e, value) => {
-    setActiveTab(value);
-  }
-
   return (
+      <BrowserRouter> 
     <div className="App">
-    <TabsContainer >
-    <Tabs selectedTab={activeTab} onChange={handleChange} >
-      <Tab label="Home" value={1}></Tab>
-      <Tab label="Alt Tab 2" value={2}></Tab>
-      <Tab label="Alt Tab 3" value={3}></Tab>
-      <Tab label="Alt Tab 4" value={4}></Tab>
-    </Tabs>
-    </TabsContainer>
-    <TabPanelContainer>
-      <TabPanel value={activeTab} selectedIndex={1}>
-      <h5>Home Page Contents</h5>
-        <p>Nothing yet</p>
-        <p>Nothing yet</p>
-      </TabPanel>
-      <TabPanel value={activeTab} selectedIndex={2}>
-      <h5>Alt Tab 2 Contents</h5>
-        <p>Nothing yet</p>
-        <p>Nothing yet</p>
-      </TabPanel>
-      <TabPanel value={activeTab} selectedIndex={3}>
-        <h5>Alt Tab 3 Contents</h5>
-        <p>Nothing yet</p>
-        <p>Nothing yet</p>
-      </TabPanel>
-      <TabPanel value={activeTab} selectedIndex={4}>
-        <h5>Alt Tab 4 Contents</h5>
-        <p>Nothing yet</p>
-        <p>Nothing yet</p>
-      </TabPanel>
-    </TabPanelContainer>
 
-    </div>
+                <Routes> 
+
+                  <Route path='/' element={<TabDisplay /> }/>
+                  <Route path='/people/:_id' element={<APIWalkerPeopleDisplay /> }/>
+                  <Route /> 
+                  <Route /> 
+
+                </Routes>
+
+        </div>
+      </BrowserRouter>
+
   );
 }
 
