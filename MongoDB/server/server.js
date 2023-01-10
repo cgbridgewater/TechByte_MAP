ba68fbffa37ab57c5c641dc221d144f43db99e18
cgbridgewater <cgbridgewater@outlook.com>
2023-01-10 14:36:41 -0800

    first commit

---
+++ MongoDB/server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const port = 8000;
+
+// req is short for request
+// res is short for response
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+// this needs to be below the other code blocks
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
