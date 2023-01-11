a9207764a24faacab2dc30963744fc37a57e2384
cgbridgewater <cgbridgewater@outlook.com>
2023-01-10 20:02:55 -0800

    correcting route

--- MongoDB/server/routes/user.routes.js
+++ MongoDB/server/routes/user.routes.js
@@ -5,5 +5,5 @@ module.exports = app => {
     app.get('/api/users/:id', UserController.findOneSingleUser);
     app.put('/api/users/:id', UserController.updateExistingUser);
     app.post('/api/users', UserController.createNewUser);
-    app.delete('/api.users/:id', UserController.deleteAnExistingUser);
+    app.delete('/api/users/:id', UserController.deleteAnExistingUser);
 }
