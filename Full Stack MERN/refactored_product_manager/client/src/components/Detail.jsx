f331fdd65c6f2d528a522e76b15b2d441c5768e2
cgbridgewater <cgbridgewater@outlook.com>
2023-01-19 00:27:44 -0800

    updating code for isValid

--- Full Stack MERN/refactored_product_manager/client/src/components/Detail.jsx
+++ Full Stack MERN/refactored_product_manager/client/src/components/Detail.jsx
@@ -7,6 +7,7 @@ const Detail = (props) => {
     const [ item, setItem ] = useState({})
     const {id} = useParams();
     const navigate = useNavigate();
+    // const[ isValid, setIsValid ] = useState(null)
 
         useEffect(() => {
             axios.get("http://localhost:8000/api/item/" + id)
@@ -14,7 +15,9 @@ const Detail = (props) => {
                 console.log(res.data);
                 setItem(res.data);
             })
-            .catch( err => console.log(err) );
+            .catch( err => {
+                console.log(err)
+            });
         }, [id]);
 
 
@@ -23,11 +26,21 @@ const Detail = (props) => {
     return (
         <div style={{backgroundColor:"darkslateblue", minHeight:"70vh"}}>
 
+            {/* { isValid ?
+                <div>
+                    <img src="https://media.tenor.com/2y6XTN-gDUIAAAAC/borat-great.gif" alt="error"/>
+                </div>
+                :
+                <h1>Fuct it</h1>
+            } */}
                 <div style={{margin:"auto", marginBottom:"10px", marginTop:"30px",padding:"30px", height:"300px", width:"900px",backgroundColor:"lightslategray", border:"3px solid darkblue", borderRadius:"15px"}}>
                     <h1>{item.title}</h1>
-                    <h4>Price: ${item.price}</h4>
+                    <h4>Price: ${item.price.toFixed(2)}</h4>
                     <p>Description: {item.description}</p>
                 </div>
+
+
+
 
                 <div style={{display:"flex", justifyContent:"space-evenly"}}>
                     <DeleteButton itemId= {id} successCallback= {() => navigate("/home")} />
