bd61cc36d7bd18918f64e713d3b52a8afd7f3a1f
cgbridgewater <cgbridgewater@outlook.com>
2023-01-26 15:12:10 -0800

    update content

--- React/routes_and_tabs/src/components/tabDisplays/PokemonGo.jsx
+++ React/routes_and_tabs/src/components/tabDisplays/PokemonGo.jsx
@@ -6,6 +6,7 @@ const PokemonGo = () => {
 
         return (
                 <div style={{minHeight:"64vh"}} className="Container">
+                    {/* section 1 */}
                     <div className="InnerBox" >
                         <div style={{display:"flex", justifyContent:"end",width:"30%"}}>
                             <img style={{height:"50%", width:"30%"}} src="https://lh3.googleusercontent.com/6Mht5q867Kt0SMUZ2wJlMwqRfLeSJSgiSAUCsTdSFwAEjNKEE5GBdlXhlqDrk70k1QDCWjOCQAi5Kf1RCffNWI4mvPogrMZVgA1Eb3Awjt8lrbY=e365-w800" alt="1" />
@@ -22,9 +23,8 @@ const PokemonGo = () => {
                             <img style={{height:"80%"}} src="https://lh3.googleusercontent.com/9FHOk79iiGEisBJxkU9smRi8CUKagEkt_yl7T7z9mEBHypSg5sblsGkv1YOxj-4vCpVbYUeo7dC6q2rxiHn9fNlcBxXGabLd7RpsNC6MHrwCRw=rw-e365-w1440" alt="pokes" />
                         </div>
                     </div>
-
-
-                    <div  className="ContentGo"  style={{display:'flex', height:"77vh", justifyContent:"space-between"}}>
+                    {/* section 2 */}
+                    <div  className="ContentGo"  style={{display:'flex', height:"75vh", justifyContent:"space-between"}}>
                         <div style={{width:"50%"}}  >
                             <div style={{display:'flex', marginTop:"20%", marginLeft:"20%"}} >
                                 <div>
@@ -42,6 +42,13 @@ const PokemonGo = () => {
                         <div>
                             <img style={{width:"100%", height:"77vh"}} src="https://lh3.googleusercontent.com/RGShDyVofSODXIJ0eQ9umAID8tCw9KdqBFrtgCEdrxjJijG1qZBryfECP9IRV1MOJhCk4Za4VYB34DE-hnPesZNNVYMwgHKs9KrNue3LNJRJuw=rw-e365-w1440" alt="catch" />
                         </div>
+                    </div>
+                    {/* section 3 */}
+                    <div >
+                        <img style={{margin:"0 auto", width:"90%"}} src="https://lh3.googleusercontent.com/Hxrm0FEgRutcRL2mhNnpHdwDLPjlXnGMotdtlzRxwtLXK2hDbhgmA_gDFRkpeYJvrC5xrkB52NM4RQrloB-Ob7IgKTpFzciYkjwTnc36HEu_XSM=rw-e365-w1440" alt="" />
+                    </div>
+                    <div >
+                        <img style={{margin:"0 auto",marginTop:"-1%", width:"90%"}}  src="https://lh3.googleusercontent.com/d3MUt9qnSpEQTo_yOpwf9wefXSrAbGeFT1uo69kPu0piUpx3dt7k99rRpaMZAM3rV-MwVSZe8sGuPHmFKnmwP-2S2jE8KnJnvXo2-MsaMa2B-hI=rw-e365-w1440" alt="" />                    
                     </div>
                 </div>
         );
