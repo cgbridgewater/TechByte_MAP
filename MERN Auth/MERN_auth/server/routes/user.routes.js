bd2424cb9f4aa3b4ecc655fc983ea5a9ef4ffb1d
cgbridgewater <cgbridgewater@outlook.com>
2023-01-25 20:32:44 -0800

    auth updates

--- MERN Auth/MERN_auth/server/routes/user.routes.js
+++ MERN Auth/MERN_auth/server/routes/user.routes.js
@@ -2,9 +2,9 @@ const UserController = require('../controllers/user.controller');
 
 module.exports = (app) => {
     // app.get('/api', UserController.index);
-    app.get('/api/user', UserController.getAll);
     app.post('/api/register', UserController.register);
     app.post('/api/login', UserController.login);
+    app.get('/api/user', authenticate, UserController.getAll);
     app.get('/api/logout', UserController.logout);
     app.get('/api/user/:id', UserController.getOne);
     // app.post('/api/user', UserController.create);
