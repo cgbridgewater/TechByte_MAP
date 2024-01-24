3bcf586a05246ba97bda9db6aa12b8cf97f84aff
cgbridgewater <cgbridgewater@outlook.com>
2024-01-24 14:26:01 -0800

    relocating files

--- Vite/sp_map/client/src/App.jsx
+++ Vite/sp_map/client/src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css'
-import { useState } from 'react'
-
 
 function App() {
 
   return (
     <>
-      <h1 className='Title'>Is a <span>Street Parking</span> Member near you?</h1>
-        <h3 className='SubTitle'>Click Around and Find Out!!</h3>
-        <h5 className='Signup'>Not on the map?<a className='SignupLink' href="#">Sign-Up!</a> <span>or</span> <a className='LoginLink' href="#">Login</a></h5>
+      
     </>
   )
 }
--- Vite/sp_map/client/src/main.jsx
+++ Vite/sp_map/client/src/main.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import 'mapbox-gl/dist/mapbox-gl.css';
-import App from './App.jsx'
+// import MapTitle from './App.jsx'
 import Map from './components/Map.jsx'
+import MapTitle from './components/MapTitle.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className='MainContainer'>
-      <App />
+      <MapTitle />
       <Map />
     </div>
   </React.StrictMode>,
