e48f5b3a3f0a04710d33d782ca936ecf350ec515
cgbridgewater <cgbridgewater@outlook.com>
2024-01-27 00:33:28 -0800

    adjusting results output to be universal for global addresses

--- Vite/sp_map/client/src/components/Autocomplete.jsx
+++ Vite/sp_map/client/src/components/Autocomplete.jsx
@@ -8,25 +8,22 @@ import '@geoapify/geocoder-autocomplete/styles/minimal.css';
 
 function Autocomplete  () {
 
-    const [value, setValue] = useState('');
-
-    function onPlaceSelect(value) {
-        setValue(value);
-        console.log(value);
-    }
-
-    function onSuggectionChange(value) {
-        setValue(value);
-        console.log("value", value);
+    function sendPlaceDetailsRequest(feature, geocoder) {
+        // console.log("FEATURE: ",feature);
+        console.log("RESULTS PACKAGE")
+        console.log("coordinates: ","[",feature.properties.lon,", ", feature.properties.lat,"]"); // the selected result
+        console.log("addres line 1: ",feature.properties.address_line1); // the selected result
+        console.log("addres line 2: ",feature.properties.address_line2); // the selected result
+        return geocoder.sendPlaceDetailsRequest(feature);
     }
 
     return (
         <div >
             <GeoapifyContext apiKey={geoapifyKey}>
                 <GeoapifyGeocoderAutocomplete
-                    value={value}
-                    placeSelect={onPlaceSelect}
-                    suggestionsChange={onSuggectionChange}
+                    placeholder="Enter address here"
+                    addDetails={true} // persists the data out
+                    sendPlaceDetailsRequestFunc={sendPlaceDetailsRequest} // function to collect the data
                 />
             </GeoapifyContext>
         </div>
