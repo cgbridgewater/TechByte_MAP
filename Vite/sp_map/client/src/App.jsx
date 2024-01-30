bef0b98b7cf3ec58dd49c1e9222c0ed2916ad197
cgbridgewater <cgbridgewater@outlook.com>
2024-01-29 20:28:09 -0800

    adding routes

--- Vite/sp_map/client/src/App.jsx
+++ Vite/sp_map/client/src/App.jsx
@@ -1,30 +1,38 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import 'mapbox-gl/dist/mapbox-gl.css';
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import MapPage from './views/MapPage.jsx';
 import Login from "./views/Login.jsx";
 import Register from "./views/Register.jsx";
 import Profile from "./views/Profile.jsx";
 import BadLink from "./views/BadLink.jsx";
+import Logout from "./views/Logout.jsx";
 
 
+function App() {
+  
+  const [authorized, setAuthorized] = useState("");
+  
+  return (
+  <BrowserRouter>
+          <Routes>
+              {/* <> */}
+                <Route path="/" default element={<MapPage/>}/>
+                <Route path="/profile" element={<Navigate to="/login"/>}/>
+                <Route path="/login" element={<Login  authorized={authorized} setAuthorized={setAuthorized}/>} />
+                <Route path="/register" element={<Register  authorized={authorized} setAuthorized={setAuthorized}/> } />
+                <Route path="/profile/:id" element={<Profile  setAuthorized={setAuthorized}/>}  />
+                <Route path="/profile/logout" element={<Logout  setAuthorized={setAuthorized}/>}  />
 
+                {/* <Route path="/profile/edit/:id" element={<ProfileEdit />}/> */} {/* Create page before activating!! */}
+                <Route path="/:bad/*" element={<Navigate to="/404-not-found"/>}/>
+                <Route path="/404-not-found" element={<BadLink/>}/>
+              {/* </> */}
+          </Routes>
+        </BrowserRouter>
+  )
+  
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-            {/* <> */}
-              <Route path="/" default element={<MapPage/>}/>
-              <Route path="/login" element={<Login />}/>
-              <Route path="/register" element={<Register />}/>
-              <Route path="/profile" element={<Profile />}/>
-              {/* <Route path="/profile/edit" element={<ProfileEdit />}/> */}
-              <Route path="/:bad/*" element={<Navigate to="/404-not-found"/>}/>
-              <Route path="/404-not-found" element={<BadLink/>}/>
-            {/* </> */}
-        </Routes>
-      </BrowserRouter>
-    </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
