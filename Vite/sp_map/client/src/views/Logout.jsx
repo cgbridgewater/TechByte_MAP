f8a87bda58b4fc497333785f03ac2be457358090
cgbridgewater <cgbridgewater@outlook.com>
2024-01-29 20:27:53 -0800

    first commit

---
+++ Vite/sp_map/client/src/views/Logout.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+const Logout = () => {
+    const navigate = useNavigate()
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/logout", {withCredentials:true})
+            .then(() => {
+                console.log("Logged Out!")
+                navigate("/")
+            })
+    }, [])
+}
+
+export default Logout;
