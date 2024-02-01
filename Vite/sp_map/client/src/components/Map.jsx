b71a5647f25961318fc7362636be4220b818da32
cgbridgewater <cgbridgewater@outlook.com>
2024-01-31 23:44:59 -0800

    add database rendering

--- Vite/sp_map/client/src/components/Map.jsx
+++ Vite/sp_map/client/src/components/Map.jsx
@@ -1,94 +1,52 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import axios from 'axios';
 
 const token = import.meta.env.VITE_MB_Mapbox_Token;
 mapboxgl.accessToken = token;
 
 function Map() {
-    const geojson = {
-        type: 'FeatureCollection',
-        features: [
-        {
-            type: 'Feature',
-            geometry: {
-            type: 'Point',
-            coordinates: [-122.483261, 45.614846]
-            },
-            properties: {
-            title: 'Mapbox',
-            description: 'Street Parking HQ',
-            JVM: 'HQ'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-            type: 'Point',
-            coordinates: [-77.032, 38.913]
-            },
-            properties: {
-            title: 'Mapbox',
-            description: 'Washington, D.C.',
-            JVM: 'Gallo'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-            type: 'Point',
-            coordinates: [-122.414, 37.776]
-            },
-            properties: {
-            title: 'Mapbox',
-            description: 'San Francisco, California',
-            JVM: 'Pato'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-            type: 'Point',
-            coordinates: [ -81.581238, 28.417665]
-            },
-            properties: {
-            title: 'Mapbox',
-            description: 'San Francisco, California',
-            JVM: 'Paloma'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-            type: 'Point',
-            coordinates: [ -82.991635, 39.9844603]
-            },
-            properties: {
-            title: 'Mapbox',
-            description: 'Rogue',
-            JVM: ''
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-            type: 'Point',
-            coordinates: [ -117.918976, 33.812511]
-            },
-            properties: {
-            title: 'Mapbox',
-            description: 'Rogue',
-            JVM: ''
-            }
-        },
-        ]
-    };
 
+    const [user, setUser] = useState([]);
     const mapContainer = useRef(null);
     const map = useRef(null);
     const [lng, setLng] = useState((Math.random() - 0.5) * 360);
     const [lat, setLat] = useState((Math.random() - 0.5) * 100);
     const [zoom, setZoom] = useState(1);
 
+    // Get all users from DB to build pins
+    useEffect(() => {
+        axios
+        .get("http://localhost:8000/api/allusers")
+        .then((res) => {
+            setUser(res.data);
+            console.log("USER DATA", user);
+        })
+        .catch((err) => {
+            console.log("Something went wrong when gathering pins!");
+        });
+    }, []);
+
+    // // HQ Pin
+    const geojson = {
+        type: 'FeatureCollection',
+        features: [
+            {
+                type: 'Feature',
+                geometry: {
+                type: 'Point',
+                coordinates: [-122.483261, 45.614846]
+                },
+                properties: {
+                title: 'Mapbox',
+                description: 'Street Parking HQ',
+                JVM: 'HQ'
+                }
+            },
+        ]
+    };
+
+    // // Initialize Map
     useEffect(() => {
         if (map.current) return; // initialize map only once
         map.current = new mapboxgl.Map({
@@ -96,70 +54,70 @@ function Map() {
         container: mapContainer.current,
         style: 'mapbox://styles/mapbox/outdoors-v12',
         center: [lng, lat],
-        zoom: zoom
+        zoom: zoom,
         });
 
-        map.current.addControl(new mapboxgl.AttributionControl({
-        customAttribution: '<a href="https://streetparking.com/" target="_blank">Street Parking</a>'
-        }));
+        // // Custom Map Footer Tag
+        map.current.addControl(
+            new mapboxgl.AttributionControl({
+                customAttribution:
+                '<a href="https://streetparking.com/" target="_blank">Street Parking</a>',
+            })
+        );
 
-        map.current.addControl(new mapboxgl.NavigationControl({
-        showCompass: true,
-        showZoom: true,
-        }));
+        // // Add in Map Controlls (Zoom and Compass)
+        map.current.addControl(new mapboxgl.NavigationControl({ showCompass: true, showZoom: true }));
 
+        // // Create On Loading "fly to" Effect
         map.current.on('load', () => {
-        map.current.flyTo({
-            center: [-98.344620, 39.494143],
-            zoom: 3.5,
-            speed: 1.6,
-            curve: 1.42,
-            easing(t) {
-            return t;
-            }
-        })
+            map.current.flyTo({
+                center: [-98.34462, 39.494143],
+                zoom: 3.5,
+                speed: 1.6,
+                curve: 1.42,
+                easing(t) {
+                return t;
+                },
+            });
+        });
 
+        // // Mark HQ Location with Custom Pin
         for (const feature of geojson.features) {
             const el = document.createElement('div');
-            if(feature.properties.JVM == "HQ"){
-                el.className = 'Marker HQ';    
-            } 
-            
-            else if(feature.properties.JVM == "Pato"){
-                el.className = 'Marker Pato';
-            }
-            
-            else if (feature.properties.JVM == "Gallo"){
-                el.className = 'Marker Gallo';
-            }
-            
-            else if (feature.properties.JVM == "Paloma"){
-                el.className = 'Marker Paloma';
-            } 
-            
-            else if (feature.properties.JVM == ""){
-                el.className = 'Marker';
-            }
-
+            el.className = "Marker HQ"
+            // Add Pins To Map
             new mapboxgl.Marker(el)
-            .setLngLat(feature.geometry.coordinates)
-            .addTo(map.current);
+                .setLngLat(feature.geometry.coordinates)
+                .addTo(map.current);
         }
-    });
-
-        map.current.on('move', () => {
-        setLng(map.current.getCenter().lng.toFixed(4));
-        setLat(map.current.getCenter().lat.toFixed(4));
-        setZoom(map.current.getZoom().toFixed(2));
-        });
-
     }, []);
 
+    // Load pins once user data is ready
+    useEffect(() => {
+    if (!map.current || user.length === 0) return; // wait until map is loaded and user data is available
+        for (const oneUser of user) {
+            const el = document.createElement('div');
+            if (oneUser.JVM === 'Pato') {
+                el.className = 'Marker Pato';
+            } else if (oneUser.JVM === 'Gallo') {
+                el.className = 'Marker Gallo';
+            } else if (oneUser.JVM === 'Paloma') {
+                el.className = 'Marker Paloma';
+            } else if (oneUser.JVM === '') {
+                el.className = 'Marker';
+            }
+            // Add Pins To Map
+            new mapboxgl.Marker(el)
+                .setLngLat(oneUser.coordinates)
+                .addTo(map.current);
+        }
+    }, [user]);
+
     return (
-        <div className='MapBox'>
-            <div id='map' ref={mapContainer} className="MapContainer" />
+        <div className="MapBox">
+            <div id="map" ref={mapContainer} className="MapContainer" />
         </div>
     );
-    }
+}
 
 export default Map;
