3b8b1e4ec1b319e1622a0980e1443cd55d48eef4
cgbridgewater <cgbridgewater@outlook.com>
2024-02-01 21:48:09 -0800

    clean up

--- Vite/sp_map/client/src/views/Login.jsx
+++ Vite/sp_map/client/src/views/Login.jsx
@@ -1,12 +1,112 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react"
+import axios from "axios"
 
-function Login() {
+function Login (props) {
+
+    const navigate = useNavigate()
+    const [ errors, setErrors ] = useState("");
+    const [ state, setState ] = useState ({
+        login: {
+            userName:"",
+            password:""
+        }
+    });;
+
+    const { login } = state;
+    const handleLoginInputs = (e) => {
+        props.setAuthorized("");
+        setState({...state, login: {...state.login, [e.target.name]: e.target.value}})
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        axios.post("http://localhost:8000/api/login", login, {withCredentials: true})
+            .then(res => { 
+                console.log(res)
+                const userId = res.data.user._id
+                console.log("UserID", userId)
+                navigate(`/profile`);
+            })
+            .catch(err =>  {
+                setErrors(err.response);
+                console.log("errors exist!", errors);
+            })
+        }
 
     return (
+        // MAIN CONTAINER
         <div className="MainContainer">
-            <h1 className='Title'>This is a test</h1>
-            <h1 className='Title'>Signin Page</h1>
-            <Link className="Title"  to="/">Get me outta here!!</Link>
+            {/* AUTH ERROR TAG */}
+            <h2 className='Unauthorized'>{props.authorized}</h2>
+            {/* LOGIN CONTAINER */}
+            <div className="LoginContainer">
+                {/* <!-- FORM WRAPPER --> */}
+                <div className="FormWrapper">
+                    {/* FORM TITLE */}
+                    <h1 className="Title">Login Here:</h1>
+                    {/* <!-- FORM SECTION  --> */}
+                    <form className="RegForm"  onSubmit={handleLogin}>
+                        {/* EMAIL */}
+                        { errors ? 
+                            <div className='InputContainer'>
+                                <label htmlFor="email" style={{color:"red"}}>< i className="fas fa-envelope fa-sm" style={{color: "red"}}></i>&nbsp; User Name:</label>
+                                <input 
+                                    type="text" 
+                                    name="email" 
+                                    id='email' 
+                                    placeholder='Invalid Login' 
+                                    onChange={handleLoginInputs}
+                                />
+                            </div>
+                            :
+                            <div className='InputContainer'>
+                                <label htmlFor="email">< i className="fas fa-envelope fa-sm" style={{color: "#C89211"}}></i>&nbsp; User Name:</label>
+                                <input 
+                                    type="text" 
+                                    name="email" 
+                                    id='email' 
+                                    placeholder='Enter An Email' 
+                                    onChange={handleLoginInputs}
+                                />
+                            </div>
+                        }
+                        {/* <!-- PASSWORD SECTION --> */}
+                        { errors? 
+                            <div className='InputContainer'>
+                                <label htmlFor="password" style={{color:"red"}}>< i className="fas fa-key fa-sm" style={{color: "red"}}></i>&nbsp; Password:</label>
+                                <input 
+                                    type="password" 
+                                    name="password" 
+                                    id='password' 
+                                    placeholder='Invalid Login' 
+                                    onChange={handleLoginInputs}
+                                />
+                            </div>
+                            :
+                            <div className='InputContainer'>
+                                <label htmlFor="password">< i className="fas fa-key fa-sm" style={{color: "#C89211"}}></i>&nbsp; Password:</label>
+                                <input 
+                                    type="password" 
+                                    name="password" 
+                                    id='password' 
+                                    placeholder='Enter Password' 
+                                    onChange={handleLoginInputs}
+                                />
+                            </div>
+                        }
+                        {/* <!-- FORM BUTTON --> */}
+                        <button type="submit" className='FormButton' >SUBMIT</button>
+                        {/* <!-- SIGN IN LINK --> */}
+                        <p className='SignIn'>Need to sign up?  <Link to="/register" className='Gold'>Register Here!</Link></p>
+                    </form>
+                    {/* <!-- END FORM SECTION --> */}
+                    {/* LINK BACK TO MAP */}
+                    <Link className="MapLink" to="/">Back To The Map</Link>
+                </div>
+                {/* END FORM CONTAINER */}
+            </div>
+            {/* END MAIN CONTAINER */}
         </div>
     )
 }
--- Vite/sp_map/client/src/views/Register.jsx
+++ Vite/sp_map/client/src/views/Register.jsx
@@ -1,12 +1,216 @@
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom'
 import Autocomplete from "../components/Autocomplete"
+import axios from 'axios';
 
-function Register () {
+function Register (props) {
+
+    const navigate = useNavigate()
+    const [ addressLine1, setAddressLine1 ] = useState("");
+    const [ addressLine2, setAddressLine2 ] = useState("");
+    const [ coordinates, setCoordinates ] = useState([])
+    const [ JVM, setJVM ] = useState("");
+    const [ errors, setErrors ] = useState("");
+    const [ emailErrors, setEmailErrors ] = useState("");
+    const [state, setState] = useState ({
+        register: {
+            userName:"",
+            email:"",
+            password:"",
+            confirmPassword:""
+        }
+    });
+
+    const {register} = state;
+    const handleRegInputs =(e) => {
+        props.setAuthorized("");
+        setState({...state, register: {...state.register,[ e.target.name]: e.target.value}})
+    }
+
+    const handleRegistration = (e) =>{
+        e.preventDefault()
+        const data ={
+            ...register,
+            coordinates,
+            addressLine1,
+            addressLine2,
+            JVM
+        }
+        axios.post("http://localhost:8000/api/register", data, {withCredentials: true})
+            .then(res => {
+                const userId = res.data.user._id
+                console.log("results", res.data.user._id)
+                navigate(`/profile`);
+            })
+            .catch(err =>  {
+                setErrors(err.response.data.error.errors);
+                console.log("errors exist!", errors);
+            })
+    }
+
+
     return (
-        <div>
-
-            <Autocomplete />
-
+        // main container
+        <div className='MainContainer'>
+            {/* Form Container */}
+            <div className='FormWrapper'>
+                {/* Form Title */}
+                <h1 className='Title'>Register Account</h1>
+                {/* Form Start */}
+                <form className="RegForm" onSubmit={handleRegistration}>
+                    {/* USER NAME */}
+                    { errors.userName ? 
+                        <div className='InputContainer'>
+                            <label htmlFor="userName" style={{color:"red"}}>< i className="fas fa-user fa-sm" style={{color: "red"}}></i>&nbsp; User Name:</label>
+                            <input 
+                                type="text" 
+                                name="userName" 
+                                id='userName' 
+                                placeholder='Enter User Name' 
+                                onChange={handleRegInputs}
+                                />
+                        </div>
+                    :
+                        <div className='InputContainer'>
+                            <label htmlFor="userName">< i className="fas fa-user fa-sm" style={{color: "#C89211"}}></i>&nbsp; User Name:</label>
+                            <input 
+                                type="text" 
+                                name="userName" 
+                                id='userName' 
+                                placeholder='Enter User Name' 
+                                onChange={handleRegInputs}
+                                />
+                        </div>
+                    }
+                    {/* EMAIL */}
+                    { errors.email ? 
+                        <div className='InputContainer'>
+                            <label htmlFor="email" style={{color:"red"}}>< i className="fas fa-envelope fa-sm" style={{color: "red"}}></i>&nbsp; Email:</label>
+                            <input 
+                                type="email" 
+                                name="email" 
+                                id='email' 
+                                placeholder='Enter An Email' 
+                                onChange={handleRegInputs}
+                            />
+                        </div>
+                    :
+                        <div className='InputContainer'>
+                            <label htmlFor="email">< i className="fas fa-envelope fa-sm" style={{color: "#C89211"}}></i>&nbsp; Email:</label>
+                            <input 
+                                type="email" 
+                                name="email" 
+                                id='email' 
+                                placeholder='Enter An Email' 
+                                onChange={handleRegInputs}
+                            />
+                        </div>
+                    }
+                    {/* ADDRESS Line 1 */}
+                    { errors.addressLine1 ? 
+                        <div className='InputContainer'>
+                            <label htmlFor="Autocomplete" style={{color:"red"}}>< i className="fas fa-map-marked-alt fa-sm" style={{color: "red"}}></i>&nbsp;  Address:</label>
+                            <Autocomplete
+                                setAddressLine1={setAddressLine1} 
+                                setAddressLine2={setAddressLine2} 
+                                setCoordinates={setCoordinates}
+                                />
+                        </div>
+                    :
+                    <div className='InputContainer'>
+                            <label htmlFor="Autocomplete">< i className="fas fa-map-marked-alt fa-sm" style={{color: "#C89211"}}></i>&nbsp;  Address:</label>
+                            <Autocomplete 
+                                setAddressLine1={setAddressLine1} 
+                                setAddressLine2={setAddressLine2} 
+                                setCoordinates={setCoordinates} 
+                            />
+                        </div>
+                    }
+                    {/* JVM TEAM SELECT */}
+                    <div className='InputContainer'>
+                        <label htmlFor="JVM">< i className="fas fa-user-friends fa-sm" style={{color: "#C89211"}}></i>&nbsp; JVM Team:</label>
+                        <select 
+                            type="text" 
+                            name="JVM" 
+                            id='JVM' 
+                            placeholder='Enter JVM Team' 
+                            onChange={(e) => setJVM(e.target.value)}
+                            value= {JVM}
+                            >
+                                <option value="">No Team Selected</option>
+                                <option value="Gallo">Team Gallo</option>
+                                <option value="Pato">Team Pato</option>
+                                <option value="Paloma">Team Paloma</option>
+                            </select>
+                    </div>
+                    {/* PASSWORD */}
+                    { errors.password ? 
+                        <div className='InputContainer'>
+                            <label htmlFor="password" style={{color:"red"}}>< i className="fas fa-key fa-sm" style={{color: "red"}}></i>&nbsp;  Password:</label>
+                            <input 
+                                type="password" 
+                                name='password' 
+                                id='password'  
+                                placeholder='Enter A Password' 
+                                onChange={handleRegInputs}
+                            />
+                        </div>
+                    :
+                        <div className='InputContainer'>
+                            <label htmlFor="password">< i className="fas fa-key fa-sm" style={{color: "#C89211"}}></i>&nbsp;  Password:</label>
+                            <input 
+                                type="password" 
+                                name='password' 
+                                id='password'  
+                                placeholder='Enter A Password' 
+                                onChange={handleRegInputs}
+                            />
+                        </div>
+                    }
+                    {/* CONFIRM PASSWORD */}
+                    { errors.confirmPassword?
+                        <div className='InputContainer'>
+                            <label htmlFor="confirmPassword" style={{color:"red"}}>< i className="fas fa-key fa-sm" style={{color: "red"}}></i>&nbsp; Confirm <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Password:</label>
+                            <input 
+                                type="password" 
+                                name='confirmPassword' 
+                                id='confirmPassword'  
+                                placeholder='Passwords Must Match!'
+                                onChange={handleRegInputs}
+                            />
+                        </div>
+                    :
+                        <div className='InputContainer'>
+                            <label htmlFor="password">< i className="fas fa-key fa-sm" style={{color: "#C89211"}}></i>&nbsp; Confirm <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Password:</label>
+                            <input 
+                                type="password" 
+                                name='confirmPassword' 
+                                id='confirmPassword'  
+                                placeholder='Confirm Password' 
+                                onChange={handleRegInputs}
+                            />
+                        </div>
+                    }
+                    {/* SUBMIT BUTTON */}
+                    <button type="submit" className='FormButton' >SUBMIT</button>
+                    {/* <!-- SIGN IN LINK --> */}
+                    <p className='SignIn'>Already a member?  <Link to="/login" className='Gold'>Login Here!</Link></p>
+                </form>
+                {/* END FORM */}
+                {/* Disclaimer */}
+                <h6 className='Disclaimer'><b>Disclaimer - </b>This info will be seen by all who view the map.</h6>
+                <h6 className='DisclaimerText'>
+                    The location will show your pin exactly as you enter it. 
+                    <br />
+                    The more detailed address you provide, the more accurate it will be.
+                </h6>
+                {/* END Disclaimer */}
+                {/* Return To Map */}
+                <Link className="MapLink" to="/">Back To The Map</Link>
+            </div>
+            {/* END Form Container */}
         </div>
+        // End Main Container
     )
 }
 
