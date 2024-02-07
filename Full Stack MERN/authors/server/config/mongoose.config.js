2c31edb9c12d82cff49e16e7183469e9adbb17e9
Chris Bridgewater <cgbridgewater@outlook.com>
2024-02-06 20:51:01 -0800

    Delete Full Stack MERN directory

--- Full Stack MERN/authors/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb://127.0.0.1:27017/author", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log("Established a connection to the database, lets Get Jiggy With It!"))
-    .catch(err => console.log("Uh Oh, Something went wrong when connecting to the database",err))
